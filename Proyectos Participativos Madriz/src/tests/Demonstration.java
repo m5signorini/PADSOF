2a352ab4507b3da7ba5cd7134f7d46655ee3371a
Martín Sánchez Signorini <m.5.signorini.5@gmail.com>
2020-03-28 17:09:24 +0100

    Guia para la demostracion

--- Proyectos Participativos Madriz/src/tests/Demonstration.java
+++ Proyectos Participativos Madriz/src/tests/Demonstration.java
@@ -18,6 +18,16 @@ public class Demonstration {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Application app = new Application();
+		/* Registrar usuario 	*/
+		/* Entrar como admin 	*/
+		/* Aceptar registro  	*/
+		/* Salir de admin    	*/
+		/* Iniciar como usuario */
+		/* Crear proyecto 		*/
+		/* Crear colectivo 		*/
+		/* Salir del todo		*/
+		/* Revivir aplicacion	*/
+		/* Controlar correccion	*/
 	}
 
 }
