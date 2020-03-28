e802a6d0ba230243752f5b405f0c822647b29d88
cesarramirez3 <48729142+cesarramirez3@users.noreply.github.com>
2020-03-28 12:37:47 +0100

    voter class

---
+++ Proyectos Participativos Madriz/src/entities/individuals/Voter.java
@@ -0,0 +1,30 @@
+package entities.individuals;
+import java.util.*;
+
+import projects.Project; 
+
+public abstract class Voter {
+	private ArrayList<Project> projects;
+	
+	public Voter() {
+		this.projects = new ArrayList<Project>();
+	}
+	
+	public ArrayList<Project> getProjects() {
+		return projects;
+	}
+	
+	public void setProjects(ArrayList<Project> projects) {
+		this.projects= projects;
+	}
+	
+	public boolean vote(Project p) {
+		if(p.getVoters().contains(this) == true) {
+			return false;
+		}
+		this.projects.add(p.support(this));
+		return true;
+	}
+	public abstract HashSet<User> count();
+
+}
