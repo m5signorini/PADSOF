15cbe7c8c3bbaf488c11690ac4df2acf949dd16c
cesarramirez3 <48729142+cesarramirez3@users.noreply.github.com>
2020-03-28 15:00:34 +0100

    Add files via upload

--- Proyectos Participativos Madriz/src/entities/individuals/Voter.java
+++ Proyectos Participativos Madriz/src/entities/individuals/Voter.java
@@ -4,21 +4,11 @@ import java.util.*;
 import projects.Project; 
 
 public abstract class Voter {
-	protected ArrayList<Project> projects;
 	
 	public Voter() {
-		this.projects = new ArrayList<Project>();
+		
 	}
-	
-	public ArrayList<Project> getProjects() {
-		return projects;
-	}
-	
-	public void setProjects(ArrayList<Project> projects) {
-		this.projects= projects;
-	}
-	
-	
+
 	public abstract boolean vote(Project p);
 	public abstract Set<User> count();
 
