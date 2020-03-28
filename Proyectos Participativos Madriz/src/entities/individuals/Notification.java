ed40de7091a6f45c2ae391a1c97334cb80966de2
cesarramirez3 <48729142+cesarramirez3@users.noreply.github.com>
2020-03-28 12:23:44 +0100

    notification class

--- Proyectos Participativos Madriz/src/entities/individuals/Notification.java
+++ Proyectos Participativos Madriz/src/entities/individuals/Notification.java
@@ -8,5 +8,28 @@ package entities.individuals;
  *
  */
 public class Notification {
+	private String title;
+	private String text;
+	
+	public Notification(String title, String text) {
+		this.title = title;
+		this.text = text;
+	}
 
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+	
 }
