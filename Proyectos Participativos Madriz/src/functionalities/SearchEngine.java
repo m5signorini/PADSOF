c79db633200144d204e221f89e1cd152b0d5b787
Martín Sánchez Signorini <m.5.signorini.5@gmail.com>
2020-03-29 16:38:11 +0200

    Searchings

--- Proyectos Participativos Madriz/src/functionalities/SearchEngine.java
+++ Proyectos Participativos Madriz/src/functionalities/SearchEngine.java
@@ -14,29 +14,142 @@ public class SearchEngine {
 	
 	private Application data;
 	
-	public SearchEngine() {
-		
+	public SearchEngine(Application data) {
+		this.data = data;
 	}
 	
-	/* Metodo que realiza una busqueda de colectivos
-	 * @param s Cadena a buscar
+	/* Method for searching collectives
+	 * @param s String to look for
+	 * @return Matched collectives
 	 */
-	public List<Collective> searchCollective(String s) {
-		
+	public List<Collective> searchCollectives(String s) {
+		if(s == null) return null;
+		List<Collective> pool = data.getCollectives();
+		List<Collective> result = new ArrayList<Collective>();
+		for(Collective c: pool) {
+			if(c.getName().contains(s)) {
+				result.add(c);
+			}
+		}
+		return result;
+	}
+	
+	/* Method for searching a user by NIF
+	 * @param nif String to look for
+	 * @return Matched user
+	 */
+	public User searchUser(String nif) {
+		if(nif == null) return null;
+		List<User> pool = data.getRegisteredUsers();
+		pool.addAll(data.getBannedUsers());
+		pool.addAll(data.getUnregisteredUsers());
+		for(User u: pool) {
+			if(u.getNif() == nif) {
+				return u;
+			}
+		}
 		return null;
 	}
 	
-	/* Metodo que realiza una busqueda de usuarios
-	 * @param s Cadena a buscar
+	/* Private method to search for a project by title given a pool of projects
+	 * @param title String that the project title must contain
+	 * @param pool List of projects in which to look for
+	 * @return List of found projects with partially matching titles
 	 */
-	public User searchUser(String s) {
-		return null;
+	private List<Project> searchProjects(String title, List<Project> pool) {
+		if(title == null || pool == null) return null;
+		List<Project> result = new ArrayList<Project>();
+		for(Project p: pool) {
+			if(p.getTitle().contains(title)) {
+				result.add(p);
+			}
+		}
+		return result;
 	}
 	
-	/* Metodo que realiza una busqueda de proyectos
-	 * @param s Cadena a buscar
+	/* Method for searching projects sent to council
+	 * @param title String to look for
+	 * @return Matched projects
 	 */
-	public Project searchProject(String s) {
-		return null;
+	public List<Project> searchSentProjects(String title) {
+		return searchProjects(title, data.getSentProjects());
+	}
+	
+	/* Method for searching projects that expired
+	 * @param title String to look for
+	 * @return Matched projects
+	 */
+	public List<Project> searchExpiredProjects(String title) {
+		return searchProjects(title, data.getExpiredProjects());
+	}
+	
+	/* Method for searching projects available to vote
+	 * @param title String to look for
+	 * @return Matched projects
+	 */
+	public List<Project> searchPublicProjects(String title) {
+		return searchProjects(title, data.getPublicProjects());
+	}
+	
+	/* Method for searching projects pending administrator approval
+	 * @param title String to look for
+	 * @return Matched projects
+	 */
+	public List<Project> searchPendingProjects(String title) {
+		return searchProjects(title, data.getPendingProjects());
+	}
+	
+	/* Method for searching projects accepted by council
+	 * @param title String to look for
+	 * @return Matched projects
+	 */
+	public List<Project> searchFinanciatedProjects(String title) {
+		return searchProjects(title, data.getFinanciatedProjects());
+	}
+	
+	/* Method for searching projects denied by the administrator
+	 * @param title String to look for
+	 * @return Matched projects
+	 */
+	public List<Project> searchDeniedProjects(String title) {
+		return searchProjects(title, data.getDeniedProjects());
+	}
+	
+	/* Method for searching projects rejected by council
+	 * @param title String to look for
+	 * @return Matched projects
+	 */
+	public List<Project> searchRejectedProjects(String title) {
+		return searchProjects(title, data.getRejectedProjects());
+	}
+	
+	/* Method for getting social projects in a pool
+	 * @param pool Pool of projects
+	 * @result List of social projects in the pool
+	 */
+	public List<Project> getSocials(List<Project> pool) {
+		if(pool == null) return null;
+		List<Project> result = new ArrayList<Project>()
+		for(Project p: pool) {
+			if(p.getClass() == Social.class) {
+				result.add(p);
+			}
+		}
+		return result;
+	}
+	
+	/* Method for getting infrastructure projects in a pool
+	 * @param pool Pool of projects
+	 * @result List of infrastructure projects in the pool
+	 */
+	public List<Project> getInfrastucturals(List<Project> pool) {
+		if(pool == null) return null;
+		List<Project> result = new ArrayList<Project>()
+		for(Project p: pool) {
+			if(p.getClass() == Infrastructural.class) {
+				result.add(p);
+			}
+		}
+		return result;
 	}
 }
