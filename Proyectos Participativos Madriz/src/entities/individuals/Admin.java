5d630babe595ddc82ad3a8e2c7039debe2cf2313
pedrou2000 <urbina2000pedro@gmail.com>
2020-03-29 18:24:12 +0200

    Admin to string

--- Proyectos Participativos Madriz/src/entities/individuals/Admin.java
+++ Proyectos Participativos Madriz/src/entities/individuals/Admin.java
@@ -7,13 +7,18 @@ package entities.individuals;
  * @author 
  *
  */
-public class Admin extends Account {
-	
+public class Admin extends Account {	
 	public Admin(String n, String p) {
 		super(n, p);
 	}
 	
-	public boolean login(String nif, String pwd) {
+	public boolean login(String pwd) {
+		if(this.pwd.equals(pwd))
+				return true;
 		return false;
+	}
+	
+	public String toString() {
+		return "Name: " + name+ ". Password: " + pwd+ ".";
 	}
 }
