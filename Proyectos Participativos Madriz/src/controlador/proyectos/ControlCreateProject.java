98f1fc549b54160f929d3fcfda6e7f42f692d7c4
cesarramirez3 <48729142+cesarramirez3@users.noreply.github.com>
2020-05-03 16:19:58 +0200

    MVC fixed

--- Proyectos Participativos Madriz/src/controlador/proyectos/ControlCreateProject.java
+++ Proyectos Participativos Madriz/src/controlador/proyectos/ControlCreateProject.java
@@ -147,4 +147,8 @@ public class ControlCreateProject implements ActionListener {
 			break;
 		}
 	}
+	
+	public void setCollectivos(List<Collective> l) {
+		this.projectView.setRepresentedCollectives(l);
+	}
 }
